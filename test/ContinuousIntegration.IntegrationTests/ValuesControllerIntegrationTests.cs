fa4f6e876b45b3be0259345e04f728e7c2f0dd1e
Louis.Lewis <louis@lewisonline.co.za>
2016-11-23 12:19:47 +0200

    feat: add integration test project and update script to run the integration tests

---
+++ test/ContinuousIntegration.IntegrationTests/ValuesControllerIntegrationTests.cs
@@ -0,0 +1,39 @@
+﻿namespace ContinuousIntegration.IntegrationTests
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Xunit;
+
+    public class ValuesControllerIntegrationTests
+    {
+        #region Fields
+
+        private const string baseUrl = "http://localhost";
+        private const string resourceUrl = "/api/values";
+
+        #endregion Fields
+
+        #region Test Methods
+
+        [Fact]
+        public async Task Can_Get_Default_Values()
+        {
+            // Arrange
+            var httpClient = new HttpClient { BaseAddress = new Uri(baseUrl) };
+            var finalResourceUrl = resourceUrl;
+
+            // Act
+            var requestResult = await httpClient.GetAsync(finalResourceUrl);
+            var content = await requestResult.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(requestResult.StatusCode, HttpStatusCode.OK);
+            Assert.Equal(@"[""value1"",""value2""]", content);
+        }
+
+        #endregion Test Methods
+    }
+}
